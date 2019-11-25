4221195dbf11aabab21a13421933b8ea05f1f97b
QuinWithOneN <jquinsmyth@gmail.com>
2019-11-25 13:20:31 -0600

    accidental master stuff

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import StudentList from './Components/Students/StudentList'
 import Footer from './Components/Footer/Footer'
 import NavBar from './Components/PageContent/NavBar'
+import GroupThanksCard from './Components/PageContent/GroupThanksCard'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <StudentList />
+      <GroupThanksCard />
       <Footer />
     </div>
   );
---
+++ src/Components/PageContent/GroupThanksCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class GroupThanksCard extends Component {
+
+
+    render() {
+        return (
+            <>
+            <h3>Special Thanks</h3>
+            <p>Special thanks to our guest speakers and instructors! We wouldn't have made it nearly as far without you!</p>
+            </>
+
+        )
+    }
+}
