38d5dfb576570d018eb525f60f9e8cb86973c917
Chandler Turner <chandlerturner@Chandlers-MacBook-Pro.local>
2019-11-25 10:46:28 -0600

    import Nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import StudentList from './Components/Students/StudentList'
 import Footer from './Components/Footer/Footer'
+import NavBar from './Components/PageContent/NavBar'
 
 function App() {
   return (
