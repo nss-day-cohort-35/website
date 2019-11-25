844c9e1b6fad528bcb5bba273a4903d03da34302
Gradi Ellis <[ellis.gradi@gmail.com]>
2019-11-25 13:39:26 -0600

    updated typewriter

--- src/Components/PageContent/HeroCard.js
+++ src/Components/PageContent/HeroCard.js
@@ -11,7 +11,7 @@ class HeroCard extends Component {
                 <div className="heroCard">
 
                     <h1 className="heroHeaderText">hello!</h1>
-                    <Typewriter
+                    <Typewriter 
                         onInit={(typewriter) => {
                             typewriter.typeString('Hello World!')
                                 .callFunction(() => {
