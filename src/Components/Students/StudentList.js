642c7d236270a18ce443999f8d33dd63031d746f
Summer Liddington <summerliddington@gmail.com>
2019-11-26 14:46:23 -0600

    added copy to about section and thanks created thanks.css

--- src/Components/Students/StudentList.js
+++ src/Components/Students/StudentList.js
@@ -21,7 +21,7 @@ export default class StudentList extends Component {
         return (
             <>
                 <div>
-                    <h2>Here be the student list and all its contents that lie within yarrr...</h2>
+                    <h2>The first half of our 6 month bootcamp journey started the same for all of us in our cohort by learning the basic foundations of web development. After presenting our frontend capstone projects of React applications, our class divided with part of us going towards frontend development focusing on UX/UI design elements and the other part diving more into server-side technology including C# and .NET . </h2>
                 </div>
                 {this.state.students.map(student => (
                     <StudentCard
