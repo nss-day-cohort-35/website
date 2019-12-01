833d8c3fce6f466affc6bd894e3eb5abbdac30f9
Sage Klein <sageklein@gmail.com>
2019-12-01 15:34:21 -0600

    madi is on there now!

--- src/Components/Students/StudentList.js
+++ src/Components/Students/StudentList.js
@@ -24,10 +24,12 @@ export default class StudentList extends Component {
                     <h2>we are unique and here's why: </h2>
                     <p>The first half of our 6 month bootcamp journey started the same for all of us in our cohort by learning the basic foundations of web development. After presenting our frontend capstone projects of React applications, our class divided with part of us going towards frontend development focusing on UX/UI design elements and the other part diving more into server-side technology including C# and .NET . </p>
                 </div>
+                <div className="student-card-container">
                 {this.state.students.map(student => (
                     <StudentCard
                         student={student} />
                 ))}
+                </div>
             </>
         )
     }
