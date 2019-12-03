2c61adea74dfb2fcf3537d7e668ab42f2b7eaabd
Summer Liddington <summerliddington@gmail.com>
2019-12-03 14:47:17 -0600

    updated more copy

--- src/Components/Students/StudentList.js
+++ src/Components/Students/StudentList.js
@@ -22,9 +22,9 @@ export default class StudentList extends Component {
         return (
             <>
                 <div>
-                    <h2 className="cohortIntroTitle">we are different than most cohorts: </h2>
-                    <p className="cohortDescription">Others stay on the same track for the entire 6-month bootcamp, but we split into two complementary focuses. We started the journey learning basic <span id="cohortBeginning"> foundations of front-end development </span> , presenting React applications as our capstone projects.
-                    Halfway through, some of us <span id="cohortFrontend"> concentrated in UI/UX design </span> and others on <span id="cohortBackend"> server-side .NET and C#. </span> Combined, we join those skillsets to create user-centric front and backend applications. </p>
+                    <h2 className="cohortIntroTitle">we are different and here's why: </h2>
+                    <p className="cohortDescription">While past cohorts have stayed together for the entire 6 month bootcamp,  halfway through we split into two complementary focuses. We started the journey as one learning <span id="cohortBeginning"> foundations of frontend development, </span> presenting React applications as our Capstone projects.
+                    Part of us went onto <span id="cohortFrontend"> concentrate on UI/UX design </span> and others on <span id="cohortBackend"> server-side .NET and C#. </span> Combined, we join those skillsets to create user-centric front and backend applications. </p>
                 </div>
                 <div className="student-card-container">
                 {this.state.students.map(student => (
