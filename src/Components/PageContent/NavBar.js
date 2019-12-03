c69dba70ee84e2a804689b430a52335335a5633a
QuinWithOneN <jquinsmyth@gmail.com>
2019-12-03 14:11:34 -0600

    wtf navbar

--- src/Components/PageContent/NavBar.js
+++ src/Components/PageContent/NavBar.js
@@ -12,14 +12,16 @@ class NavBar extends Component {
                         alt="Cohort 35"
                         src="./images/cohort35_logo_navbar-01.png"
                     />
+                    <div className="demoDayInfo">
                         <div className="demoDayDateContainer">
                             <h3 className="demoDayDate">Nashville Software School Demo Day: February 14, 2020</h3>
                         </div>
                         <div className="demoDayRegisterContainer">
-                        <button className="demoDayRegister">
-                            <h2 className="registerText">Register</h2>
-                        </button>
+                            <button className="demoDayRegister">
+                                <h2 className="registerText">Register</h2>
+                            </button>
                         </div>
+                    </div>
 
                 </nav>
             </div>
