f5a7f0ca56a5a4e1b182c022adf4bc4edfe55b16
Gradi Ellis <[ellis.gradi@gmail.com]>
2019-12-03 13:32:47 -0600

    thank you card styled

--- src/Components/PageContent/GroupThanksCard.js
+++ src/Components/PageContent/GroupThanksCard.js
@@ -7,27 +7,27 @@ export default class GroupThanksCard extends Component {
     render() {
         return (
             <>
-            <div>
-                <h2>Special Thanks to: </h2>
-                <h3>Our guest speakers</h3>
+            <div className="thanksContainer">
+                <h2 className="thankYouTitle">Special Thanks to: </h2>
+                <h3 className="speakersInfo">Our guest speakers:</h3>
                 <ul>
-                    <li>Gina Antonini</li>
-                    <li>Lauren Atkins</li>
-                    <li>Rachel Babcock</li>
-                    <li>Allison Collins</li>
-                    <li>Jase Hackman</li>
-                    <li>Richard Lancaster</li>
-                    <li>Dillian Teagle</li>
-                    <li>Rachel Vawter</li>
-                    <li>Gretchen Ward</li>
-                    <li>JD Wheeler</li>
-                    <li>Trevon Young</li>
+                    <li className="speakerNames">Gina Antonini</li>
+                    <li className="speakerNames">Lauren Atkins</li>
+                    <li className="speakerNames">Rachel Babcock</li>
+                    <li className="speakerNames">Allison Collins</li>
+                    <li className="speakerNames">Jase Hackman</li>
+                    <li className="speakerNames">Richard Lancaster</li>
+                    <li className="speakerNames">Dillian Teagle</li>
+                    <li className="speakerNames">Rachel Vawter</li>
+                    <li className="speakerNames">Gretchen Ward</li>
+                    <li className="speakerNames">JD Wheeler</li>
+                    <li className="speakerNames">Trevon Young</li>
                 </ul>
-                <h3>and our wonderful teachers and instructors</h3>
-                    <li>Brenda Long</li>
-                    <li>Mo</li>
-                    <li>Madi</li>
-                    <li>Adam</li>
+                <h3 className="speakersInfo">...and our wonderful teachers and instructors</h3>
+                    <li className="speakerNames">Brenda Long</li>
+                    <li className="speakerNames">Mo</li>
+                    <li className="speakerNames">Madi</li>
+                    <li className="speakerNames">Adam</li>
             </div>
             </>
 
