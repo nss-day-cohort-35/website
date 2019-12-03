3d33952d7c64230f5518c0a6d97c9564ec12be05
Summer Liddington <summerliddington@gmail.com>
2019-12-03 13:51:38 -0600

    updated copy

--- src/Components/Students/StudentList.js
+++ src/Components/Students/StudentList.js
@@ -22,11 +22,9 @@ export default class StudentList extends Component {
         return (
             <>
                 <div>
-                    <h2 className="cohortIntroTitle">we are unique and here's why: </h2>
-                    <p className="cohortDescription">The first half of our 6 month bootcamp
-                    journey started the same for all of us in our cohort by learning the basic foundations
-                    of web development. After presenting our frontend capstone projects of React applications,
-                    our class divided with part of us going towards <span id="cohortFrontend"> frontend development focusing on UX/UI design elements </span> and the other part diving more into <span id="cohortBackend"> server-side technology including .NET and #C. </span> </p>
+                    <h2 className="cohortIntroTitle">we are different than most cohorts: </h2>
+                    <p className="cohortDescription">Others stay on the same track for the entire 6-month bootcamp, but we split into two complementary focuses. We started the journey learning basic <span id="cohortBeginning"> foundations of front-end development </span> , presenting React applications as our capstone projects.
+                    Halfway through, some of us <span id="cohortFrontend"> concentrated in UI/UX design </span> and others on <span id="cohortBackend"> server-side .NET and C#. </span> Combined, we join those skillsets to create user-centric front and backend applications. </p>
                 </div>
                 <div className="student-card-container">
                 {this.state.students.map(student => (
