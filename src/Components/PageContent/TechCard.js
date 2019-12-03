3aeffe95b2ac9c6f3a60c3b86b593766e799fe4e
QuinWithOneN <jquinsmyth@gmail.com>
2019-12-03 15:03:52 -0600

    prep for master merge

--- src/Components/PageContent/TechCard.js
+++ src/Components/PageContent/TechCard.js
@@ -5,9 +5,9 @@ class TechCard extends Component {
     render() {
         return (
             <section className="techSection">
-            <h2 className="technologiesIntroTitle">technologies used:</h2>
-                <div id="firstHalfTech">
+            <h2 className="technologiesIntroTitle">technologies and skills:</h2>
                 <h4 className="technologiesTitleText">First 12 Weeks</h4>
+                <div id="firstHalfTech">
                     <a href="https://www.javascript.com/" className="techAll">JavaScript</a>
                     <a href="https://github.com/" className="techAll">Github</a>
                     <a href="https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5" className="techAll">HTML5</a>
@@ -22,7 +22,7 @@ class TechCard extends Component {
                 </div>
                 <hr className="technologiesLine"></hr>
                 <div id="backendTech">
-                <h4 className="technologiesTitleText">Full Stack Tech</h4>
+                <h4 className="technologiesTitleText">Full Stack</h4>
                     <a href="https://docs.microsoft.com/en-us/dotnet/csharp/" className="techBack">C#</a>
                     <a href="https://www.microsoft.com/net/" className="techBack">.NET Core</a>
                     <a href="https://www.microsoft.com/en-us/sql-server/sql-server-2017" className="techBack">SQL Server</a>
@@ -31,7 +31,7 @@ class TechCard extends Component {
                 </div>
                 <hr className="technologiesLine"></hr>
                 <div id="frontendTech">
-                <h4  className="technologiesTitleText">UX/UI Tech</h4>
+                <h4  className="technologiesTitleText">UX/UI</h4>
                     <a href="https://www.figma.com/" className="techFront">Figma</a>
                     <a href="https://sass-lang.com/" className="techFront">SASS</a>
                     <a href="https://firebase.google.com/" className="techFront">Firebase</a>
